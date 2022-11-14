7bbb98a61ea4925b141006feff08757c86b34d7d
Kisho Mahadevan <kisokumar@fb.com>
2022-11-14 23:57:13 +0000

    feat: add reps/weight done

--- src/models/Exercise.js
+++ src/models/Exercise.js
@@ -3,6 +3,8 @@ const { db, DataTypes } = require("../db/db.js");
 const Exercise = db.define("excercises", {
   id: { type: DataTypes.INTEGER, primaryKey: true },
   name: DataTypes.STRING,
+  repsDone: DataTypes.INTEGER,
+  weightDone: DataTypes.INTEGER,
   muscleGroup: DataTypes.STRING,
 });
 
