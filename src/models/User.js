17e676674defe8af865651f2ec595c9d28c86a58
Kisho Mahadevan <kisokumar@fb.com>
2022-11-15 05:14:17 +0000

    fix: user model attributes

--- src/models/User.js
+++ src/models/User.js
@@ -2,8 +2,11 @@ const { db, DataTypes } = require("../db/db");
 
 const User = db.define("users", {
   id: { type: DataTypes.INTEGER, primaryKey: true },
-  username: DataTypes.STRING,
-  password: DataTypes.STRING,
+  username: { type: DataTypes.STRING, allowNull: false },
+  password: { type: DataTypes.STRING, allowNull: false },
+  email: { type: DataTypes.STRING, allowNull: false },
+  firstname: { type: DataTypes.STRING, allowNull: true },
+  lastname: { type: DataTypes.STRING, allowNull: true },
 });
 
 module.exports = { User };
