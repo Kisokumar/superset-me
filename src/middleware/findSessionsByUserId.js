1c3db4e14bc3f8041edbbc8b6615f59ebcee707b
Kisho Mahadevan <kisokumar@fb.com>
2022-11-15 04:44:38 +0000

    feat: find session using user id

---
+++ src/middleware/findSessionsByUserId.js
@@ -0,0 +1,14 @@
+const { Session } = require("../models");
+
+async function findSessionByUserId(req, res, next) {
+  req.sessions = await Session.findAll({
+    where: { userId: req.params.userId },
+  });
+  if (!req.sessions) {
+    res.status(500).json("Session does not exist!");
+  } else {
+    next();
+  }
+}
+
+module.exports = findSessionByUserId;
