d9e9dddcb4852046be7089fa32431ae22b6c51a7
Kisho Mahadevan <kisokumar@fb.com>
2022-11-17 11:08:23 +0000

    feat: generate unique id

---
+++ src/middleware/uniqueIdGenerator.js
@@ -0,0 +1,10 @@
+function uniqueId() {
+  return (
+    Date.now().toString(36) +
+    Math.floor(
+      Math.pow(10, 12) + Math.random() * 9 * Math.pow(10, 12)
+    ).toString(36)
+  );
+}
+
+module.exports = uniqueId;
