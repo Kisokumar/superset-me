197890940f424b71c642266b628360cc071bc6eb
Kisho Mahadevan <kisokumar@fb.com>
2022-11-17 11:13:43 +0000

    feat: moving to uuid

--- src/middleware/uniqueIdGenerator.js
+++ src/middleware/uniqueIdGenerator.js
@@ -1,10 +1,7 @@
+const { v4: uuidv4 } = require("uuid");
+
 function uniqueId() {
-  return (
-    Date.now().toString(36) +
-    Math.floor(
-      Math.pow(10, 12) + Math.random() * 9 * Math.pow(10, 12)
-    ).toString(36)
-  );
+  return uuidv4();
 }
 
 module.exports = uniqueId;
