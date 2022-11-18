575db7d35c03fff1511b37a218a239e04274e1c6
Kisho Mahadevan <kisokumar@fb.com>
2022-11-18 22:58:53 +0000

    fix: model takes in friends,age vars + username is unique

--- src/models/User.js
+++ src/models/User.js
@@ -8,11 +8,13 @@ const User = db.define("users", {
     primaryKey: true,
     defaultValue: Sequelize.UUIDV4,
   },
-  username: { type: DataTypes.STRING, allowNull: false },
+  username: { type: DataTypes.STRING, allowNull: false, unique: true },
   password: { type: DataTypes.STRING, allowNull: false },
   email: { type: DataTypes.STRING, allowNull: false },
   firstname: { type: DataTypes.STRING, allowNull: true },
   lastname: { type: DataTypes.STRING, allowNull: true },
+  age: { type: DataTypes.INTEGER, allowNull: true },
+  friends: { type: DataTypes.STRING, allowNull: true },
 });
 
 module.exports = { User };
