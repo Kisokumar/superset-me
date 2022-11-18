2cbb6d7fe9081921a81e1b999e8fcbfda864af68
Kisho Mahadevan <kisokumar@fb.com>
2022-11-18 22:55:00 +0000

    fix: moving server

---
+++ src/server/server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const { db } = require("../db/db");
+
+const sessionRouter = require("../routes/sessions");
+const userRouter = require("../routes/users");
+const friendRouter = require("../routes/friends");
+
+const app = express();
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  var header = "<h3>Welcome to the Superset.me API!</h3>";
+  var subheading = "<h4>Available endpoints:</h4>";
+  var userendpoints =
+    "<h9>User endpoints:</h9><p> GET /users/ - Get all the users</p><p>GET /users/:userId - Get user by id</p><p>GET /users/username/:username - Get user by username</p> <p>GET /users/userId/sessions - Get sessions by userId</p> <p>GET /users/username/:username/sessions - Get sessions by username</p>";
+  const page = header + subheading + userendpoints;
+  res.send(page);
+});
+
+app.use("/sessions", sessionRouter);
+app.use("/users", userRouter);
+app.use("/friends", friendRouter);
+
+module.exports = app;
