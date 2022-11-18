4b6d927fea8f55f5424dfe2a503d81ffe98afae2
Kisho Mahadevan <kisokumar@fb.com>
2022-11-18 23:17:40 +0000

    build: config file

--- src/db/db.js
+++ src/db/db.js
@@ -1,6 +1,17 @@
 const { Sequelize, DataTypes } = require("sequelize");
 
+// local
+/*
 const db = new Sequelize("database", "username", "password", {
+  dialect: "sqlite",
+  storage: "./src/db/gym-tracker.sqlite",
+  logging: false,
+});
+*/
+
+// external
+const db = new Sequelize("database", "username", "password", {
+  host: config.host,
   dialect: "sqlite",
   storage: "./src/db/gym-tracker.sqlite",
   logging: false,
