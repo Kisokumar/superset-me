96fade831ed99a9ea19d33fa5793dd870e293ff8
Kisho Mahadevan <kisokumar@fb.com>
2022-11-19 05:49:29 +0000

    build: `hashPassword` + `validatePassword`

--- src/middleware/password.js
+++ src/middleware/password.js
@@ -1,11 +1,14 @@
 const bcrypt = require("bcrypt");
 const saltRounds = Math.floor(Math.random() * 8) + 6; //rng between 8-12
 
-console.clear();
-
 function hashPassword(plainTextPassword) {
   const hash = bcrypt.hashSync(plainTextPassword, saltRounds);
   return hash;
 }
 
-module.exports = hashPassword;
+function validatePassword(plainTextPassword) {
+  const hash = bcrypt.hashSync(plainTextPassword, saltRounds);
+  return hash;
+}
+
+module.exports = { hashPassword, validatePassword };
