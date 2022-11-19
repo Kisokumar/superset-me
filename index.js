7415aee3cde7ed1546d97f872a7b89c58dae0549
Kisho Mahadevan <kisokumar@fb.com>
2022-11-19 04:47:15 +0000

    feat: external db

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ const server = require("./src/server/server");
 port = 3000;
 
 server.listen(port, () => {
-  console.log(`App listening on ${port}`);
+  console.log(`  -  App listening on ${port}`);
 });
--- src/db/db.js
+++ src/db/db.js
@@ -1,20 +1,33 @@
+// external db
 const { Sequelize, DataTypes } = require("sequelize");
+const config = require("./config.json")["dev"];
 
-// local
-/*
-const db = new Sequelize("database", "username", "password", {
-  dialect: "sqlite",
-  storage: "./src/db/gym-tracker.sqlite",
-  logging: false,
-});
-*/
+location = "local";
 
-// external
-const db = new Sequelize("database", "username", "password", {
-  host: config.host,
-  dialect: "sqlite",
-  storage: "./src/db/gym-tracker.sqlite",
-  logging: false,
-});
+if (location == "local") {
+  const localdb = new Sequelize("database", "username", "password", {
+    dialect: "sqlite",
+    storage: "./src/db/gym-tracker.sqlite",
+    logging: false,
+  });
+
+  console.log(`  -  Currently using local sqlite db`);
+  db = localdb;
+} else if (location == "external") {
+  const externaldb = new Sequelize(
+    config.database,
+    config.user,
+    config.password,
+    {
+      port: config.port,
+      host: config.server,
+      dialect: config.dialect,
+      logging: false,
+    }
+  );
+
+  console.log(`  -  Currently using external ${config.dialect} db`);
+  db = externaldb;
+}
 
 module.exports = { db, DataTypes };
