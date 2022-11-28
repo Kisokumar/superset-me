440071e9132950b63c7a67365650eeeed43066eb
Kisho Mahadevan <kisokumar@fb.com>
2022-11-28 05:08:45 +0000

    styles: refactoring `css` + now using flex

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,11 @@
 import Header from "./components/Header";
-// import Footer from "./components/Footer";
 import Register from "./components/Register";
+// import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">{Header}</header>
+      <header className="App-header">{Header()}</header>
       <main>{Register()}</main>
     </div>
   );
